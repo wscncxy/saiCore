2a4fe897b61f0a9a956ed2fbd2791c44b7782595
wscncxy <wscncxy@gmail.com>
2018-07-23 23:04:25 +0800

    基础架子快好了

--- src/main/java/com/sai/core/constants/Constants.java
+++ src/main/java/com/sai/core/constants/Constants.java
@@ -23,8 +23,22 @@ public class Constants {
     public static final Byte SAI_PROGRAM_INDEX_MANAGE = 3;
     public static final Byte SAI_PROGRAM_INDEX_SHOPPING = 4;
 
+
+    public static final Integer SAI_MENU_TYPE_TOPMENU = 0;  //顶部菜单
+    public static final Integer SAI_MENU_TYPE_SUBMENU = 1;  //子菜单
+    public static final Integer SAI_MENU_TYPE_PAGEMENU = 2;  //页面菜单
+    public static final Integer SAI_MENU_TYPE_BUTTONMENU = 3; //按钮菜单
+    public static final Integer SAI_MENU_TYPE_HIDEMENU = 4; //隐藏菜单，ex: ajax请求
+
+    public static final String SAI_MENU_TYPE_NAME_TOPMENU = "顶部菜单";  //顶部菜单
+    public static final String SAI_MENU_TYPE_NAME_SUBMENU = "子菜单";  //子菜单
+    public static final String SAI_MENU_TYPE_NAME_PAGEMENU = "页面菜单";  //页面菜单
+    public static final String SAI_MENU_TYPE_NAME_BUTTONMENU = "按钮菜单"; //按钮菜单
+    public static final String SAI_MENU_TYPE_NAME_HIDEMENU = "隐藏菜单"; //隐藏菜单，ex: ajax请求
+
     public static Map<Byte, String> SAI_PROGRAM_INDEX_MAP = new HashMap<>();
     public static Map<String, Byte> SAI_PROGRAM_MAP = new HashMap<>();
+    public static Map<Integer, String> SAI_MENU_TYPE_MAP = new HashMap<>();
 
     static {
         SAI_PROGRAM_INDEX_MAP.put(SAI_PROGRAM_INDEX_OPENAPI, SAI_PROGRAM_OPENAPI);
@@ -37,5 +51,11 @@ public class Constants {
             Map.Entry<Byte, String> entry = iterator.next();
             SAI_PROGRAM_MAP.put(entry.getValue(), entry.getKey());
         }
+
+        SAI_MENU_TYPE_MAP.put(SAI_MENU_TYPE_TOPMENU, SAI_MENU_TYPE_NAME_TOPMENU);
+        SAI_MENU_TYPE_MAP.put(SAI_MENU_TYPE_SUBMENU, SAI_MENU_TYPE_NAME_SUBMENU);
+        SAI_MENU_TYPE_MAP.put(SAI_MENU_TYPE_PAGEMENU, SAI_MENU_TYPE_NAME_PAGEMENU);
+        SAI_MENU_TYPE_MAP.put(SAI_MENU_TYPE_BUTTONMENU, SAI_MENU_TYPE_NAME_BUTTONMENU);
+        SAI_MENU_TYPE_MAP.put(SAI_MENU_TYPE_HIDEMENU, SAI_MENU_TYPE_NAME_HIDEMENU);
     }
 }
