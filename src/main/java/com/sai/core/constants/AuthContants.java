4a521a3543a0a0092482eb6d47afb44bd10b9eb0
wscncxy@gamil.com <wscncxy@gamil.com>
2018-12-25 20:01:03 +0800

    update spring boot to 2.x

--- src/main/java/com/sai/core/constants/AuthContants.java
+++ src/main/java/com/sai/core/constants/AuthContants.java
@@ -3,8 +3,8 @@ package com.sai.core.constants;
 /**
  * Created by ZhouXiang on 2017/11/3 0003 14:16.
  */
-public class AuthConstants {
-    public static final String AUTH_FRONT_TOKEN_KEY = "DKCS-HT-AT";
+public class AuthContants {
+    public static final String AUTH_TOKEN_KEY = "AUTH_TOKEN";
     public static final String AUTH_USER_MENU_KEY_PRE = "AUTH_USER_MENU_KEY_PRE";
 
     public static final String PROGRAM_BE = "BE";
--- src/main/java/com/sai/core/constants/StatusConstant.java
+++ src/main/java/com/sai/core/constants/StatusConstant.java
@@ -3,8 +3,10 @@ package com.sai.core.constants;
 public class StatusConstant {
     public static final String RESULT_SUCCESS_CODE="0000";
     public static final String RESULT_FAIL_CODE="9999";
+    public static final String RESULT_AUTH_FAIL_CODE="4041";
 
     public static final String RESULT_SUCCESS_MSG="处理成功";
     public static final String RESULT_FAIL_MSG="处理失败";
+    public static final String RESULT_AUTH_FAIL_MSG="权限不足";
 
 }
